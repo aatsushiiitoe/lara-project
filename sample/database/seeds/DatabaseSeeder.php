051b28b65e4c0ef2552e9f9d5ed996f296ef84c6
at <newfrog98@gmail.com>
2019-11-07 15:39:33 +0700

    hoge

--- sample/database/seeds/DatabaseSeeder.php
+++ sample/database/seeds/DatabaseSeeder.php
@@ -10,7 +10,8 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
-    }
+{
+     // BooksTableSeederを読み込むように指定
+     $this->call(BooksTableSeeder::class);
+}
 }
